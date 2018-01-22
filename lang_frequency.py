2c12b841f7a841266b4f001833f70566250ece68
Daneev <30390132+Daneev@users.noreply.github.com>
2018-01-22 15:04:39 +0400

    декоратор

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,5 +1,8 @@
 import itertools
-import os, re
+import os
+import re
+import sys
+
 
 def load_data(filepath):
 
@@ -11,25 +14,36 @@ def load_data(filepath):
         return file.read()
 
 
-def get_most_frequent_words(text):
-    list_word = [re.sub(r'[^\w\s]+', '', s) for s in text.split()]
-    result = {i: list_word.count(i) for i in list_word}
-    result.pop('')
-    #print(list_word)
-    #print(result)
-    #print(list(result.keys())[list(result.values()).index(54)])
-    #result1 = sorted(result.items(), key=lambda x: x[1], reverse=True)
-    #print(result1[:10])
-    #print(sorted((value, key) for (key, value) in result.items(), reverse=True))
-    #print(sorted(result, key=result.get, reverse=True))
-    #for w in sorted(result, key=result.get, reverse=True):
-        #print(result[w], w)
+def removing_punctuation(text):
+    list_clr_word = [re.sub(r'[^\w\s]+', '', s) for s in text.split()]
+    return list_clr_word
 
-    world = (itertools.islice([(result[w], w) for w in sorted(result, key=result.get, reverse=True)], 10))
-    for x, y in list(world): print(x, y)
+
+def create_dict_freq_word(list_clr_word):
+    dict_freq_word = {i: list_clr_word.count(i) for i in list_clr_word}
+    dict_freq_word.pop('')
+    return dict_freq_word
+
+
+def get_most_frequent_words(text):
+    dict_word_result = create_dict_freq_word(removing_punctuation(text))
+    ten_frequent_words = (itertools.islice(
+        [(dict_word_result[w], w) for w in sorted(dict_word_result, key=dict_word_result.get, reverse=True)], 10))
+    return ten_frequent_words
+
+
+def print_words(words):
+    for x, y in list(words): print(x, y)
+    return None
+
+def get_path_data():
+    if len(sys.argv) > 1:
+        return sys.argv[1]  # возвращаем путь к файлу, если программу запустили с параметрами
+
+    print("Введите путь до файла:")
+    return input()  # если программа запущена без параметров,
+    # ожидаем ввода пути к файлу с клавиатуры.
 
 
 if __name__ == '__main__':
-    print(get_most_frequent_words(load_data('data_for_test/text.txt')))
-
-
+    print_words(get_most_frequent_words(load_data(get_path_data())))
